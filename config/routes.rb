8ce76226c7354f78acf24413a1fd851c0a5a7e1b
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-10-14 18:18:50 +0900

    ランディングページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/lp', to: 'landing_pages#index'
+  root to: 'landing_pages#index'
 end
