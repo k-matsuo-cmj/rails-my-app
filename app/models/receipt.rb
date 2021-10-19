269c7282822c330aa10fcc2142e248460286dbe7
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-10-19 15:33:28 +0900

    fix validations

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -2,4 +2,5 @@ class Receipt < ApplicationRecord
   has_many :receipt_details, inverse_of: :receipt, dependent: :destroy
   accepts_nested_attributes_for :receipt_details
   validates :total_price, numericality: { greater_than: 0 }
+  validates :receipt_details, presence: true
 end
--- app/models/receipt_detail.rb
+++ app/models/receipt_detail.rb
@@ -1,4 +1,6 @@
 class ReceiptDetail < ApplicationRecord
   belongs_to :receipt
   validates :name, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :quantity, numericality: { greater_than: 0 }
 end
