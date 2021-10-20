27b10c9e7c3981a67b9c2d2dc1c3e2c5d84aeba2
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-10-20 20:17:24 +0900

    modal sign in / sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'charts/index'
   devise_for :users
   get '/lp', to: 'landing_pages#index'
-  root to: 'landing_pages#index'
+  root to: 'receipts#index'
   resources :receipts, only: [:index, :new, :create, :edit, :update, :destroy]
 end
